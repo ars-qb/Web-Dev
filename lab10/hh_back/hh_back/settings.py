15fe3d82f7e45014368495fc2a783315e14628ec
ars-qb <arssai.main@gmail.com>
2024-04-25 17:03:35 +0500

    commit

--- lab10/hh_back/hh_back/settings.py
+++ lab10/hh_back/hh_back/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-7je)qi=7714yrj%*dp#xx8#8+38#i@pn$(4u21h&+s^k)k%+b!
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["localhost.charlesproxy.com", "localhost"]
 
 
 # Application definition
